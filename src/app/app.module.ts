88d880b919401c87e50624f00a4a4dab03e42644
PC-MORGAN <morgan.g@mbo-informatique.com>
2022-02-09 16:47:48 +0100

    rbzebs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {GaugeModule} from "angular-gauge";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    GaugeModule.forRoot(),
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTabsModule,
+    RouterModule.forRoot([]),
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
